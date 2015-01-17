4dd33523d39bea16e6e12dc9075b729427264628
Brian Mikolajczyk <brianm12@gmail.com>
2015-01-17 10:48:55 -0500

    Renamed program; Added header and version in stringinpi.py

--- stringinpi.py
+++ stringinpi.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
-from sympy.mpmath import *
-import re
+#String in Pi, stringinpi.py v1.0
+#Copyright (c) 2015 by Brian Mikolajczyk, brianm12@gmail.com
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
+import re
+from sympy.mpmath import *
 import sys
 
 class color:
@@ -52,5 +68,7 @@ def intcheck(val):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('string', type=intcheck)
+    parser.add_argument('--version', action='version',\
+                         version='stringinpi.py v1.0')
 
     main(**vars(parser.parse_args()))
